2fd732e79e68fb1a37372934cb16620bb16b3487
Mikhail <mixandoma@ya.ru>
2017-10-16 17:12:52 +0500

    database integration added

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, redirect, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+import database_entry
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_entry.database_credentials
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    comment = db.Column(db.String(2000))
 
 
 @app.route("/")
@@ -19,7 +30,13 @@ def view():
 def posts():
     name = request.form['name']
     post = request.form['post']
-    return render_template('view.html', name=name, post=post)
+
+    signature = Posts(name=name, comment=post)
+    db.session.add(signature)
+    db.session.commit()
+
+    result = Posts.query.all()
+    return render_template('view.html', result=result)
 
 
 if __name__ == '__main__':
