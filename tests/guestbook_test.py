04de8dba1f49e43f5124909e9c1cd45613e67286
Mikhail <mixandoma@ya.ru>
2017-11-03 20:58:43 +0500

    testing skeleton

---
+++ tests/guestbook_test.py
@@ -0,0 +1,19 @@
+import os, unittest, tempfile
+import guestbook as gb
+
+class GuestTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, gb.app.config['DATABASE'] = tempfile.mkstemp()
+        gb.app.testing = True
+        self.app = gb.app.test_client()
+        with gb.app.app_context():
+            gb.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(gb.app.config['DATABASE'])
+
+
+if __name__ == '__main__':
+    unittest.main()
