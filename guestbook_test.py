600962757770bc1ef502c1c47cc907acb9728953
Mikhail <mixandoma@ya.ru>
2017-11-03 21:09:44 +0500

    test included

---
+++ guestbook_test.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+import tempfile
+import guestbook.py as gb
+
+
+class GuestTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, gb.app.config['DATABASE'] = tempfile.mkstemp()
+        gb.app.testing = True
+        self.app = gb.app.test_client()
+        with gb.app.app_context():
+            gb.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(gb.app.config['DATABASE'])
+
+
+if __name__ == '__main__':
+    unittest.main()
